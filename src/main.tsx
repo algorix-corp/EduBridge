5955593a46d75140a06d69a79be0df94013d13bd
issac4892 <issac4892@gmail.com>
2023-08-19 03:55:19 +0900

    :sparkles: Building Main and adding + academy routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import './index.css';
 import { MantineProvider } from '@mantine/core';
 import App from './App.tsx';
+import { Header } from './components/Header.tsx';
+import { BuildingHeader } from './components/BuildingHeader.tsx';
+import { CreateBuilding } from './components/CreateBuilding.tsx';
 import { DatesProvider } from '@mantine/dates';
+import { BuildingMain } from './components/BuildingMain.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: (
       <>
+        <Header />
         <App />
         <Outlet />
       </>
@@ -18,17 +22,40 @@ const router = createBrowserRouter([
   },
   {
     path: '/building',
-    element: <></>,
+    element: (
+      <>
+        <BuildingHeader />
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        path: '',
+        element: <BuildingMain />,
+      },
+      {
+        path: 'new',
+        element: <CreateBuilding />,
+      },
+    ],
   },
-  {},
+  {
+    path: '/academy',
+    element: (
+      <>
+        <AcademyHeader />
+        <Outlet />
+      </>
+    ),
+    children: [
+
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <DatesProvider settings={{ firstDayOfWeek: 0 }}>
-        <RouterProvider router={router} />
-      </DatesProvider>
-    </MantineProvider>
-  </React.StrictMode>,
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <DatesProvider settings={{ firstDayOfWeek: 0 }}>
+      <RouterProvider router={router} />
+    </DatesProvider>
+  </MantineProvider>,
 );
