aca17872e5d0990ea5bd34f05503a62d39715163
Ryan Kwon <kznm.develop@gmail.com>
2023-08-19 05:26:02 +0900

    run server as [uvicorn app.main:app --reload]

--- app/main.py
+++ app/main.py
@@ -110,9 +110,3 @@ def startup():
 
     app.include_router(academy.router)
     app.include_router(building.router)
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
