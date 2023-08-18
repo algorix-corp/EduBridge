58f736e47034bf5248c0407a1d34cd42bdcbc656
unsignd <unsigndid@gmail.com>
2023-08-19 01:42:54 +0900

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export function Header() {
+  return <Container></Container>;
+}
+
+const Container = styled.div``;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <Outlet />
+      </>
+    ),
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
