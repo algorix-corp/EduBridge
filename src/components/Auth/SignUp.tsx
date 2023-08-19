76555f7f28bf2a718c513fcde644dbb47ba11375
unsignd <unsigndid@gmail.com>
2023-08-19 20:41:04 +0900

    ✏️ typo

--- src/components/Auth/SignUp.tsx
+++ src/components/Auth/SignUp.tsx
@@ -106,7 +106,7 @@ export function SignUp() {
             placeholder="Choose your role"
             defaultValue={'building-owner'}
             data={[
-              { value: 'admin', label: 'Admin' }
+              { value: 'admin', label: 'Admin' },
               { value: 'building', label: 'Building Owner' },
               { value: 'academy', label: 'Academy' },
             ]}
