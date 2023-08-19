d539a09fb9ac2455ec9198167ae5fc35541bd16e
Ryan Kwon <kznm.develop@gmail.com>
2023-08-19 21:18:47 +0900

    :recycle: Refactor routes/auth.py

--- api/routes/auth.py
+++ api/routes/auth.py
@@ -9,29 +9,28 @@ router = APIRouter(
 )
 
 
-class UserIn(BaseModel):
+class UserLogin(BaseModel):
     email: str
     password: str
 
 
 @router.post("/login")
-# def login(user: UserIn):
-#     with Session(engine) as session:
-#         # user = session.get(User, user.email)
-#         user = session.query(User).filter(User.email == user.email).first()
-#         if not user:
-#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-#         if not checkpw(user.password.encode(), user.password.encode()):
-#             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong password")
-#         return issue_auth_token(user.id)
-def login(user: UserIn):
+def login(user: UserLogin):
     with Session(engine) as session:
-        res = session.query(User).filter(User.email == user.email).first()
-        if not res:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-        if not checkpw(user.password.encode(), res.password.encode()):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong password")
-        return {"Bearer": issue_auth_token(res.id)}
+        user = session.query(User).filter(User.email == user.email).first()
+        if user:
+            if checkpw(user.password.encode(), user.password_hash.encode()):
+                return {"Bearer": issue_auth_token(user.id)}
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Invalid email or password",
+                )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid email or password",
+            )
 
 
 @router.post("/auth")
