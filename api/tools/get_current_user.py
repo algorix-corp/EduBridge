d60ddd35120ae059a24982ffdcfed90ba63152e5
Ryan Kwon <kznm.develop@gmail.com>
2023-08-19 13:15:46 +0900

    ✅ Test code! (btw doesn't work though)

--- api/tools/get_current_user.py
+++ api/tools/get_current_user.py
@@ -4,12 +4,16 @@ from fastapi.security import HTTPBearer
 from api.tools.database import engine, Session
 from api.schemas.user import User
 
+import api.tools.env
+import os
+
 
 def get_current_user(token=Depends(HTTPBearer())):
     try:
-        payload = jwt.decode(token.credentials, "secret", algorithms=["HS256"])
+        payload = jwt.decode(token.credentials, os.getenv("JWT_SECRET"), algorithms=["HS256"])
         with Session(engine) as session:
-            user = session.get(User, payload["email"])
+            # user = session.get(User, payload.email)
+            user = session.query(User).filter(User.email == payload["email"]).first()
             if not user:
                 raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
         return payload
