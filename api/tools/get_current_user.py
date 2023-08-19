82312fedaf5f2a51ab42f7491279c0b4a10e73ec
Ryan Kwon <kznm.develop@gmail.com>
2023-08-19 11:05:09 +0900

    ✅ /user done

---
+++ api/tools/get_current_user.py
@@ -0,0 +1,17 @@
+from jose import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer
+from database import engine, Session
+from api.schemas.user import User
+
+
+def get_current_user(token=Depends(HTTPBearer())):
+    try:
+        payload = jwt.decode(token.credentials, "secret", algorithms=["HS256"])
+        with Session(engine) as session:
+            user = session.get(User, payload["email"])
+            if not user:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        return payload
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
