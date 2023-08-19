d617f45129adc6e268e4fa64db5bccd91bf0ed91
unsignd <unsigndid@gmail.com>
2023-08-19 21:24:07 +0900

    pull issue

--- src/components/Academy/AcademyStudentsDashboard.tsx
+++ src/components/Academy/AcademyStudentsDashboard.tsx
@@ -1,34 +1,88 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styled from 'styled-components';
 import { Grid } from 'gridjs-react';
 import 'gridjs/dist/theme/mermaid.css';
 
-const DashboardContainer = styled.div`
-  margin: 0 auto;
-  width: 80%;
-  max-width: 1200px;
-`;
-
 export function AcademyStudentsDashboard() {
   const [studentStatuses] = useState([
-    { studentName: '일론머스크', subject: 'Math', paymentStatus: 'Paid', description: '도지도지' },
-    { studentName: '주커버그', subject: 'History', paymentStatus: 'Pending', description: '메-타' },
-    { studentName: '시진핑', subject: 'Science', paymentStatus: 'Paid', description: '핑핑이' },
-    { studentName: '일론머스크', subject: 'Math', paymentStatus: 'Paid', description: '도지도지' },
-    { studentName: '주커버그', subject: 'History', paymentStatus: 'Pending', description: '메-타' },
-    { studentName: '시진핑', subject: 'Science', paymentStatus: 'Paid', description: '핑핑이' },
-    { studentName: '일론머스크', subject: 'Math', paymentStatus: 'Paid', description: '도지도지' },
-    { studentName: '주커버그', subject: 'History', paymentStatus: 'Pending', description: '메-타' },
-    { studentName: '시진핑', subject: 'Science', paymentStatus: 'Paid', description: '핑핑이' },
-    { studentName: '일론머스크', subject: 'Math', paymentStatus: 'Paid', description: '도지도지' },
-    { studentName: '주커버그', subject: 'History', paymentStatus: 'Pending', description: '메-타' },
-    { studentName: '시진핑', subject: 'Science', paymentStatus: 'Paid', description: '핑핑이' },
+    {
+      studentName: '일론머스크',
+      subject: 'Math',
+      paymentStatus: 'Paid',
+      description: '도지도지',
+    },
+    {
+      studentName: '주커버그',
+      subject: 'History',
+      paymentStatus: 'Pending',
+      description: '메-타',
+    },
+    {
+      studentName: '시진핑',
+      subject: 'Science',
+      paymentStatus: 'Paid',
+      description: '핑핑이',
+    },
+    {
+      studentName: '일론머스크',
+      subject: 'Math',
+      paymentStatus: 'Paid',
+      description: '도지도지',
+    },
+    {
+      studentName: '주커버그',
+      subject: 'History',
+      paymentStatus: 'Pending',
+      description: '메-타',
+    },
+    {
+      studentName: '시진핑',
+      subject: 'Science',
+      paymentStatus: 'Paid',
+      description: '핑핑이',
+    },
+    {
+      studentName: '일론머스크',
+      subject: 'Math',
+      paymentStatus: 'Paid',
+      description: '도지도지',
+    },
+    {
+      studentName: '주커버그',
+      subject: 'History',
+      paymentStatus: 'Pending',
+      description: '메-타',
+    },
+    {
+      studentName: '시진핑',
+      subject: 'Science',
+      paymentStatus: 'Paid',
+      description: '핑핑이',
+    },
+    {
+      studentName: '일론머스크',
+      subject: 'Math',
+      paymentStatus: 'Paid',
+      description: '도지도지',
+    },
+    {
+      studentName: '주커버그',
+      subject: 'History',
+      paymentStatus: 'Pending',
+      description: '메-타',
+    },
+    {
+      studentName: '시진핑',
+      subject: 'Science',
+      paymentStatus: 'Paid',
+      description: '핑핑이',
+    },
     // ... (other initial student data)
   ]);
 
   const columns = ['Student Name', 'Subject', 'Payment Status', 'Description'];
 
-  const data = studentStatuses.map((status) => [
+  const data = studentStatuses.map(status => [
     status.studentName,
     status.subject,
     status.paymentStatus,
@@ -36,7 +90,7 @@ export function AcademyStudentsDashboard() {
   ]);
 
   return (
-    <DashboardContainer>
+    <Container>
       <Grid
         data={data}
         columns={columns}
@@ -45,6 +99,19 @@ export function AcademyStudentsDashboard() {
         sort={true}
         className="table"
       />
-    </DashboardContainer>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+
+  position: relative;
+  top: 100px;
+
+  margin: 0 auto;
+  width: 100vw;
+  height: calc(100vh - 100px);
+  max-width: 1200px;
+`;
