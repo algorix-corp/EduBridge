2ebf141af7b52a4ab757046cc3fc76721fa7abd7
Ryan Kwon <kznm.develop@gmail.com>
2023-08-19 10:36:50 +0900

    api/tools refactor + migrate scheme

--- api/main.py
+++ api/main.py
@@ -1,2 +1,4 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+import api.tools.env
---
+++ api/tools/database.py
@@ -0,0 +1,11 @@
+import os
+
+from sqlmodel import create_engine
+
+db_host = os.getenv("POSTGRES_HOST")
+db_name = os.getenv("POSTGRES_DB")
+db_user = os.getenv("POSTGRES_USER")
+db_pass = os.getenv("POSTGRES_PASSWORD")
+
+database_url = f"postgresql://{db_user}:{db_pass}@{db_host}/{db_name}"
+engine = create_engine(database_url)
