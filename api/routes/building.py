65097701e1ed2ecf86cbae592c3d632367549c80
Ryan Kwon <kznm.develop@gmail.com>
2023-08-19 21:16:15 +0900

    :recycle: Refactor routes/building.py

--- api/routes/building.py
+++ api/routes/building.py
@@ -6,43 +6,53 @@ router = APIRouter(
 )
 
 
-class BuildingIn(BaseModel):
-    name: str
-    address: str
+class BuildingCreate(BaseModel):
+    name: str = Field(min_length=3)
+    address: str = Field(min_length=3)
     description: Optional[str] = None
     image_url: Optional[str] = None
 
 
 class BuildingUpdate(BaseModel):
-    name: Optional[str]
-    address: Optional[str]
+    name: Optional[str] = Field(min_length=3)
+    address: Optional[str] = Field(min_length=3)
     description: Optional[str] = None
     image_url: Optional[str] = None
 
 
 @router.post("/")
-def create_building(building: BuildingIn, current_user=Depends(get_current_user)):
-    if current_user.role != "building" or current_user.role != "admin":
+def create_building(building: BuildingCreate, current_user=Depends(get_current_user)):
+    if current_user.role != "admin" and current_user.role != "building":
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
+
     with Session(engine) as session:
         building = Building(**building.dict(), owner_id=current_user.id)
         session.add(building)
         session.commit()
         session.refresh(building)
-        return building
+        return {"message": "Building created successfully", "building": building}
 
 
 @router.get("/")
 def get_buildings(current_user=Depends(get_current_user)):
-    with Session(engine) as session:
-        buildings = session.query(Building).filter(Building.owner_id == current_user.id).all()
-        return buildings
+    if current_user.role == "admin":
+        with Session(engine) as session:
+            buildings = session.query(Building).all()
+            return buildings
+
+    elif current_user.role == "building":
+        with Session(engine) as session:
+            buildings = session.query(Building).filter(Building.owner_id == current_user.id).all()
+            return buildings
+
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
 
 
 @router.get("/{building_id}")
 def get_building(building_id: int, current_user=Depends(get_current_user)):
     with Session(engine) as session:
-        building = session.query(Building).filter(Building.id == building_id).first()
+        building = session.get(Building, building_id)
         if not building:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Building not found")
         return building
@@ -51,18 +61,18 @@ def get_building(building_id: int, current_user=Depends(get_current_user)):
 @router.put("/{building_id}")
 def update_building(building_id: int, building: BuildingUpdate, current_user=Depends(get_current_user)):
     with Session(engine) as session:
-        building = session.query(Building).filter(Building.id == building_id).first()
+        building = session.get(Building, building_id)
         if not building:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Building not found")
-        if building.owner_id != current_user.id:
+        if building.owner_id != current_user.id and current_user.role != "admin":
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
-        for key, value in building.dict().items():
-            if value is not None:
-                setattr(building, key, value)
-        session.add(building)
+        building_data = building.dict()
+        update_data = building.dict(exclude_unset=True)
+        updated_building = Building(**building_data, **update_data)
+        session.add(updated_building)
         session.commit()
-        session.refresh(building)
-        return building
+        session.refresh(updated_building)
+        return {"message": "Building updated successfully", "building": updated_building}
 
 
 @router.delete("/{building_id}")
@@ -71,7 +81,7 @@ def delete_building(building_id: int, current_user=Depends(get_current_user)):
         building = session.query(Building).filter(Building.id == building_id).first()
         if not building:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Building not found")
-        if building.owner_id != current_user.id:
+        if building.owner_id != current_user.id and current_user.role != "admin":
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
         session.delete(building)
         session.commit()
